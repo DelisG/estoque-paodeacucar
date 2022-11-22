3aa59617ae72104efbccba908420409a21155378
Delis Gloria <delisemarciano@gmail.com>
2022-11-21 23:27:45 -0300

    :pencil: Implementando formulários de edição

---
+++ editar.php
@@ -0,0 +1,79 @@
+<?php
+
+$nome = $_POST['nome'] ?? "";
+$descricao = $_POST['descricao'] ?? "";
+$preco = $_POST['preco'] ?? "";
+$data = $_POST['data'] ?? "";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema Pão de Açucar</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg bg-info">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">Pão de Açucar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="index.php">Lista de produtos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="cadastro.php">Cadastrar Produto</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container">
+
+        <h1 class="mt-4">Editar Produto: <?php echo $nome; ?></h1>
+
+
+        <form action="editar.php" method="post">
+
+            <div>
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Nome</label>
+                    <input type="text" class="form-control" id="exampleFormControlInput1" name="nome" value="<?php echo $nome; ?>">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleFormControlTextarea1" class="form-label">Descrição</label>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="descricao"> <?php echo $descricao; ?></textarea>
+                </div>
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Preço</label>
+                    <input type="number" class="form-control" id="exampleFormControlInput1" name="preco" value="<?php echo $preco; ?>">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Data</label>
+                    <input type="date" class="form-control" id="exampleFormControlInput1" name="data" value="<?php echo $data; ?>">
+                </div>
+                <div class="mb-3">
+                    <button type="submit" class="btn btn-success">Adicionar</button>
+                </div>
+            </div>
+
+        </form>
+
+
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
